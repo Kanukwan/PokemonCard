2e841f1529b60a0dd5f7b31a7a714a0998bc911c
P.ADA <haseao112@gmail.com>
2021-08-08 16:47:32 +0700

    change send param to crate vs dom by map

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import AppHeader from './components/AppHeader';
 import Pokemon from './components/Pokemon';
 // import PokemonPost from './PokemonPost';
 
-const pokemon1 = {
+const pokemons = [
+  {
     title: "Dex1-1",
     thumbnailUrl:"/images/dex1-1.png"
-};
+  },
+  {
+    title: "Dex1-2",
+    thumbnailUrl:"/images/dex1-2.png"
+  },
+  {
+    title: "Dex1-3",
+    thumbnailUrl:"/images/dex1-3.png"
+  },
+];
 
 
 function App() {
+
+  const pokemonElement = pokemons.map((pokemon, index) => {
+    return <Pokemon key={index} pokemon={pokemon}/>;
+  })
+
   return (
     <div className="app">
         <AppHeader/>
         <div className="app-grid"> 
-          <Pokemon pokemon={pokemon1}/>
+          {pokemonElement}
         </div>
     </div>
   );
