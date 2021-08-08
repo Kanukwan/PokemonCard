42ef4fb0ce3b7b07f1c762181cfe13f666a45a5a
P.ADA <haseao112@gmail.com>
2021-08-08 19:45:30 +0700

    split array pokemon data object to pokemon.ja an app.js use them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css';
 import AppHeader from './components/AppHeader';
 import Pokemon from './components/Pokemon';
+import pokemons from './data/pokemons';
 // import PokemonPost from './PokemonPost';
-
-const pokemons = [
-  {
-    title: "Dex1-1",
-    thumbnailUrl:"/images/dex1-1.png"
-  },
-  {
-    title: "Dex1-2",
-    thumbnailUrl:"/images/dex1-2.png"
-  },
-  {
-    title: "Dex1-3",
-    thumbnailUrl:"/images/dex1-3.png"
-  },
-];
-
 
 function App() {
 
---
+++ src/data/pokemons.js
@@ -0,0 +1,40 @@
+const pokemons = [
+    {
+        title: "Dex1-1",
+        thumbnailUrl:"/images/dex1-1.png"
+    },
+    {
+        title: "Dex1-2",
+        thumbnailUrl:"/images/dex1-2.png"
+    },
+    {
+        title: "Dex1-3",
+        thumbnailUrl:"/images/dex1-3.png"
+    },
+    {
+        title: "Dex2-1",
+        thumbnailUrl:"/images/dex2-1.png"
+    },
+    {
+        title: "Dex2-2",
+        thumbnailUrl:"/images/dex2-2.png"
+    },
+    {
+        title: "Dex2-3",
+        thumbnailUrl:"/images/dex2-3.png"
+    },
+    {
+        title: "Dex3-1",
+        thumbnailUrl:"/images/dex3-1.png"
+    },
+    {
+        title: "Dex3-2",
+        thumbnailUrl:"/images/dex3-2.png"
+    },
+    {
+        title: "Dex3-3",
+        thumbnailUrl:"/images/dex3-3.png"
+    },
+];
+
+export default pokemons;
